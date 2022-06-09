fc9b0d6f7348fb754c53659476ca542e9c3216d7
DBS20 <d.bojorquezs150619@gmail.com>
2022-06-09 14:31:17 -0500

    appSGuide: colors

---
+++ appFlutter-SGuide/lib/utilerias/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class GlobalColors{
+  //Colores
+  static const Color colorWhite = const Color(0xffF5F5F5);
+  static const Color colorWhiteCream = const Color(0xffE8E8E8);
+  static const Color colorGrey = const Color(0xff2F2F3B);
+  static const Color colorBlack = const Color(0xff000000);
+  static const Color colorBlueRaf = const Color(0xff6985C0);
+  static const Color colorBlue = const Color(0xff0D7CCE);
+  static const Color colorBlueCapri = const Color(0xff19C7DF);
+  static const Color colorGreenTurquoise = const Color(0x6a19DFCD);
+  static const Color colorGreenLight = const Color(0x6a26F298);
+  static const Color colorGreenEmerald= const Color(0xff189392);
+  static const Color colorBrown= const Color(0xff866a6a);
+  static const Color colorBeigeBackground = const Color(0xfff6eae0);
+}
