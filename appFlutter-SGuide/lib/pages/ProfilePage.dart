c6356a8992034acd55dd5ae8ee690921ef63d81f
DBS20 <d.bojorquezs150619@gmail.com>
2022-06-14 18:01:11 -0500

    appSGuide: profile
    -diseño

--- appFlutter-SGuide/lib/pages/ProfilePage.dart
+++ appFlutter-SGuide/lib/pages/ProfilePage.dart
@@ -17,84 +17,98 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                //
-              },
-              icon: Icon(Icons.person)),
-        ],
         title: Text('Perfil',
             style: GoogleFonts.nunito(
                 fontSize: 20,
                 fontStyle: FontStyle.italic,
-                color: Colors.white,
+                color: GlobalColors.colorWhite,
                 fontWeight: FontWeight.bold)),
         backgroundColor: GlobalColors.colorGrey,
         shadowColor: GlobalColors.colorGreenLight,
         elevation: 20,
       ),
       body: Container(
-        color: GlobalColors.colorLightGrey,
+        color: GlobalColors.colorDarkGrey,
         child: Center(
           child: Column(
             children: [
-             Image.network("lib/logo/Wallpaper2.jpg"),
 
-              Card(
-                color: GlobalColors.colorBlueRaf,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                margin: EdgeInsets.all(10),
+             Image.network("lib/logo/profileWallpaper.jpg"),
+
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+
                 child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
-                        title: Text('Nombre completo:'),
-                        subtitle: Text('Diana Bojorquez Soto'),
-                        leading: Icon(Icons.person),
+                  children: [
+                    Card(
+                      color: GlobalColors.colorGrey,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(1)),
+                      margin: EdgeInsets.all(1),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ListTile(
+                              title: Text('Nombre completo:', style: GoogleFonts.nunito(fontSize: 18,color: GlobalColors.colorGreenTurquoise,fontWeight: FontWeight.bold)),
+                              subtitle: Text('Diana Bojorquez Soto',style: GoogleFonts.nunito(fontSize: 15,color: GlobalColors.colorGreenTurquoise)),
+                              leading: Icon(Icons.alternate_email_sharp, size: 50, color: GlobalColors.colorGreenTurquoise),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Divider(
+                      thickness: 0.3,
+                      color: GlobalColors.colorGreenLight,
+                    ),
+
+                    Card(
+                      color: GlobalColors.colorGrey,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(1)),
+                      margin: EdgeInsets.all(1),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ListTile(
+                              title: Text('Correo:', style: GoogleFonts.nunito(fontSize: 18,color: GlobalColors.colorGreenTurquoise,fontWeight: FontWeight.bold)),
+                              subtitle: Text('dbs70624@udelasalle.edu.mx', style: GoogleFonts.nunito(fontSize: 15,color: GlobalColors.colorGreenTurquoise)),
+                              leading: Icon(Icons.mail, size: 50, color: GlobalColors.colorGreenTurquoise),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Divider(
+                      thickness: 0.3,
+                      color: GlobalColors.colorGreenLight,
+                    ),
+
+                    Card(
+                      color: GlobalColors.colorGrey,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(1)),
+                      margin: EdgeInsets.all(1),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ListTile(
+                              //contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+                              title: Text('Matricula:', style: GoogleFonts.nunito(fontSize: 18,color: GlobalColors.colorGreenTurquoise,fontWeight: FontWeight.bold)),
+                              subtitle: Text('70624', style: GoogleFonts.nunito(fontSize: 15,color: GlobalColors.colorGreenTurquoise)),
+                              leading: Icon(Icons.apps_sharp, size: 50,color: GlobalColors.colorGreenTurquoise),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-              Card(
-                color: GlobalColors.colorBlueRaf,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                margin: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
-                        title: Text('Correo:'),
-                        subtitle: Text('dbs70624@udelasalle.edu.mx'),
-                        leading: Icon(Icons.mail),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Card(
-                color: GlobalColors.colorBlueRaf,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                margin: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
-                        title: Text('Matricula:'),
-                        subtitle: Text('70624'),
-                        leading: Icon(Icons.apps_sharp),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+
+
             ],
           ),
         ),
